af9a66aec2145e707ef3929016ed3cbc5caab0dd
agusvlopez <agusvlopez@gmail.com>
2024-02-03 13:34:14 -0300

    add: features_section with swiper

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,6 +15,10 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="<?= url('css/style.css') ?>">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 </head>
 
 <body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -60,5 +60,193 @@
                 </div>
             </div>
         </section>
+        {{-- PARA DESPUES --}}
+        {{-- <section class="rooms__section">
+            <div class="rooms__section-info">
+                <p>ROOMS</p>
+                <h1>Hand Picked Rooms</h1>
+            </div>
+
+            <!-- Slider main container -->
+            <div class="swiper">
+                <div class="swiper-wrapper">
+                    <!-- Slides -->
+                    @foreach ($rooms as $room)
+                    <div class="swiper-slide">
+                        <div class="rooms__section-amenities">
+                            @foreach ($room['amenityImages'] as $image)
+                            {!! $image !!}
+                            @endforeach
+                        </div>
+                        <div class="slider-images">
+                            <img src="{{$room['randomImage']}}" alt="room image" />
+                        </div>
+                        <div class="rooms__section-price">
+                            <a href="../rooms-grid">
+                                <div class="rooms__section-price-info">
+                                    <h1>{{$room['room_type']}}</h1>
+                                    <p>{{$room['description']}}</p>
+                                </div>
+                                <div class="rooms__section-price-number">
+                                    <span class='{{ $room["discount"] ? "price-low-number" : "price-number-big" }}'>$ {{$room['discountedPrice']}} /night</span>
+                                    <!-- <span class='{{ $room["discount"] ? "price-low-text" : "price-high-text" }}'> /night</span> -->
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+                <div class="swiper-button-prev"><img src="/img/home/img-slider-left-arrow.png" alt="left arrow"></div>
+                <div class="swiper-button-next"><img src="/img/home/img-slider-right-arrow.png" alt="right arrow"></div>
+            </div>
+        </section> --}}
+        <section class="video__section">
+            <div class="video__section-content">
+                <div class="video__section-content-info">
+                    <h2>Intro video</h2>
+                    <h3>Meet With Our Luxury Place.</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat you have to understand this.</p>
+                </div>
+                <div class="video__section-content-iframe">
+                    <iframe src="https://www.youtube.com/embed/Bu3Doe45lcU?start=25" frameborder="0"
+                        allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="video__section-button">
+                <a href="rooms-grid">Book Now</a>
+            </div>
+        </section>
+        <section class="features__section">
+            <div class="features__section-info">
+                <h2>Facilities</h2>
+                <h3>Core Features</h3>
+            </div>
+            <div class="swiper-container features__section-cards-container">
+                <div class="swiper-wrapper cards-container">
+                    <div class="swiper-slide card card1">
+                        <div class="card-img">
+                            <img src="/assets/home/icon_facilities-1.svg" alt="">
+                        </div>
+                        <h4>Have High Rating</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna..</p>
+                        <span class="card-number">01</span>
+                        <span class="card-elipses">
+                            <img src="/assets/home/elipse-brown.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                        </span>
+                    </div>
+
+                    <div class="swiper-slide card card2">
+                        <div class="card-img">
+                            <img src="/assets/home/icon_facilities-2.png" alt="">
+                        </div>
+                        <h4>Quiet Hours</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna..</p>
+                        <span class="card-number">02</span>
+                        <span class="card-elipses">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-brown.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                        </span>
+                    </div>
+                    <div class="swiper-slide card card3">
+                        <div class="card-img">
+                            <img src="/assets/home/icon_facilities-3.png" alt="">
+                        </div>
+                        <h4>Best Locations</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna..</p>
+                        <span class="card-number">03</span>
+                        <span class="card-elipses">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-brown.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                        </span>
+                    </div>
+                    <div class="swiper-slide card card4">
+                        <div class="card-img">
+                            <img src="/assets/home/icon_facilities-4.png" alt="">
+                        </div>
+                        <h4>Free Cancellation</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna..</p>
+                        <span class="card-number">04</span>
+                        <span class="card-elipses">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-brown.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                        </span>
+                    </div>
+                    <div class="swiper-slide card card5">
+                        <div class="card-img">
+                            <img src="/assets/home/icon_facilities-5.png" alt="">
+                        </div>
+                        <h4>Payment Options</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna..</p>
+                        <span class="card-number">05</span>
+                        <span class="card-elipses">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-brown.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                        </span>
+                    </div>
+                    <div class="swiper-slide card card6">
+                        <div class="card-img">
+                            <img src="/assets/home/icon_facilities-6.png" alt="">
+                        </div>
+                        <h4>Special Offers</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna..</p>
+                        <span class="card-number">06</span>
+                        <span class="card-elipses">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-white.svg" alt="">
+                            <img src="/assets/home/elipse-brown.svg" alt="">
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 @endsection
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        // Verificar si el dispositivo es móvil
+        console.log(window.matchMedia("(max-width:1024px)").matches);
+        if (window.matchMedia("(max-width: 1024px)").matches) {
+            var swiper = new Swiper('.swiper-container', {
+
+                slidesPerView: 'auto', // Muestra la cantidad de slides que quepan en el contenedor
+                spaceBetween: 20, // Espacio entre las tarjetas
+                loop: true, // Activa el bucle infinito
+                // Más opciones de configuración según tus necesidades
+                centeredSlides: true,
+            });
+        }
+    });
+</script>
